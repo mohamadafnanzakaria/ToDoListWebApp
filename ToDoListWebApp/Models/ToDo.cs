9ed6d3c111f6dda74ada7b9b1470b2c3d37624b6
Mohamad Afnan Bin Zakaria <mohamad.bin.zakaria@avanade.com>
2023-09-12 14:32:31 +0800

    Add project files.

---
+++ ToDoListWebApp/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoListWebApp.Models
+{
+    public class ToDo
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Task { get; set; }
+
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public Status Status { set; get; } = Status.Created;
+    }
+}
